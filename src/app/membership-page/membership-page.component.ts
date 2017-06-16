c5d11a0894e81c0699906cccd318c557c15e8496
peter <pkarella27@yahoo.com>
2017-06-16 14:09:23 -0700

    figured out mock sit

--- src/app/membership-page/membership-page.component.ts
+++ src/app/membership-page/membership-page.component.ts
@@ -9,19 +9,12 @@ import { MemberService } from '../member.service';
   providers: [MemberService]
 })
 export class MembershipPageComponent implements OnInit {
-  members: Member[] = [
-     new Member("member", "George Washington", 22,
-         "A great guy who is rumored to have never told a lie", 1),
-     new Member("member", "Richard Nixon", 45,
-         "He will always pick, but never tust a book choice from Mr. slick Dick", 2),
-     new Member("member", "Jimmy Carter", 50,
-         "Whatever you do don't talk about his peanuts or else he becomes a mean nut", 3)
-    ];
+  members: Member[];
 
   constructor(private router: Router, private memberService: MemberService){}
 
   ngOnInit(){
-
+    this.members = this.memberService.getMembers(); 
     }
 
   goToDetailPage(clickedMember: Member) {
